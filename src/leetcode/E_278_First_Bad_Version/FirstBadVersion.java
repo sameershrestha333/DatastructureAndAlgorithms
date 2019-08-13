ad60271541efbfaf81ae9dce656428309faeeb52
sameershrestha333 <sameershrestha333@gmail.com>
2019-08-13 17:40:43 -0400

    278. First Bad Version
    https://leetcode.com/problems/first-bad-version/

---
+++ src/leetcode/E_278_First_Bad_Version/FirstBadVersion.java
@@ -0,0 +1,23 @@
+package leetcode.E_278_First_Bad_Version;
+
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int mid) {
+
+        return false;
+    }
+}
